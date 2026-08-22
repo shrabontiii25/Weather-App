ecb70de6a3f4733957ffbdc2bb91384aea027aba
Shrabonti Majumder <smaj0010@student.monash.edu>
2026-08-22 17:02:41 +0800

    feat: update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../components/pages/HomePage.vue';
 import DetailPage from '../components/pages/DetailPage.vue';
+import EditProfilePage from '../components/pages/EditProfilePage.vue';
 
 const routes = [
   { path: '/', name: 'home', component: HomePage },
   { path: '/city/:cityName', name: 'detail', component: DetailPage },
+  { path: '/profile', name: 'profile', component: EditProfilePage },
 ];
 
 const router = createRouter({
